db9e037ad31c923e5ba72acc8797091410e53e3c
Ariel Aguilar <ariel@goja.com>
2022-08-13 01:44:47 -0400

    consume status endpoint

--- src/app/item-detail/item-detail.component.ts
+++ src/app/item-detail/item-detail.component.ts
@@ -5,29 +5,49 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-item-detail',
   templateUrl: './item-detail.component.html',
-  styleUrls: ['./item-detail.component.css']
+  styleUrls: ['./item-detail.component.css', '../app.component.css']
 })
 export class ItemDetailComponent implements OnInit {
   detail: any;
-  detail$: Observable<any>;
+  detailStatus: any;
   isLoading: boolean;
+  isLoadingStatus: boolean;
+  isReady: boolean;
   isLoading$: Observable<boolean>;
+  isLoadingStatus$: Observable<boolean>;
+  isReady$: Observable<boolean>;
+  detail$: Observable<any>;
+  detailStatus$: Observable<any>;
 
   constructor(public downloadFileService :DownloadFileService) {
-    this.detail$ = this.downloadFileService.getDetail$();
     this.isLoading = false;
+    this.isLoadingStatus = false;
+    this.isReady = false;
     this.isLoading$ = this.downloadFileService.getIsLoading$();
+    this.isLoadingStatus$ = this.downloadFileService.getIsLoadingStatus$();
+    this.isReady$ = this.downloadFileService.getIsReady$();
+    this.detail$ = this.downloadFileService.getDetail$();
+    this.detailStatus$ = this.downloadFileService.getDetailStatus$();
   }
 
   ngOnInit(): void {
-    this.detail$ = this.downloadFileService.getDetail$();
-    this.detail$.subscribe(detail => this.detail = detail);
-    this.isLoading$ = this.downloadFileService.getIsLoading$();
+    // this.isLoading$ = this.downloadFileService.getIsLoading$();
+    // this.isReady$ = this.downloadFileService.getIsReady$();
+    // this.detail$ = this.downloadFileService.getDetail$();
+    // this.detailStatus$ = this.downloadFileService.getDetailStatus$();
     this.isLoading$.subscribe(isLoading => this.isLoading = isLoading);
+    this.isLoadingStatus$.subscribe(isLoadingStatus => this.isLoadingStatus = isLoadingStatus);
+    this.isReady$.subscribe(isReady => this.isReady = isReady);
+    this.detail$.subscribe(detail => this.detail = detail);
+    this.detailStatus$.subscribe(detailStatus => this.detailStatus = detailStatus);
   }
 
   download(): void {
     console.log('descargando ...');
+  }
+
+  prepareDownload() {
+    this.downloadFileService.statusProcess();
   }
 
 }
